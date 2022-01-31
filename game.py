85b7a1dac53d0e36f89acb995f94e8158be993ae
jaydeshmukh14 <97574516+jaydeshmukh14@users.noreply.github.com>
2022-01-31 17:35:54 -0500

    v1

--- game.py
+++ game.py
@@ -5,3 +5,7 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+import random
+options = ["rock","paper","scissors"]
+computer_choice = random.choice(options)
+print("computer choose:", computer_choice)
